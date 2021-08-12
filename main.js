8f76750c93d1faa5b07e3f8b0ca8de4286921d4c
OGA <totti0927.oga@gmail.com>
2021-08-12 12:21:29 +0900

    計算式追加

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 function clearScreen(){
-  console.log('aaa');
   document.getElementById('result').value = "";
 };
 
 function display(value) {
   document.getElementById('result').value += value;
+}
+
+function calc() {
+  result.value = new Function("return " + result.value)();
 }
